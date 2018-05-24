acb943bfae9ea5133c74502ec3f3ca3fd358e683
Diaz Octavio <odiaz@lcs-mba3082.local>
2018-05-23 20:19:16 -0400

    input restrictions added, nil code done.  Trying to make averga

--- DealOrNoDeal/main.swift
+++ DealOrNoDeal/main.swift
@@ -8,6 +8,25 @@ import Foundation
 // INPUT
 // Global variable to use later in program
 var countOfBriefcasesOpened = 3
+while 1 == 1 {
+    print("How many briefcases have been opened?")
+    
+    guard let givenInput = readLine() else{
+        //promt again if nil
+        continue
+    }
+    
+    guard let givenInteger = Int(givenInput) else{
+        continue
+    }
+    
+    if givenInteger < 0 || givenInteger > 10{
+        continue
+    }
+    
+    countOfBriefcasesOpened = givenInteger
+    break
+}
 
 // Now get the actual number of briefcases that were opened during the game
 // e.g.: write the rest of the INPUT section
@@ -17,9 +36,23 @@ var countOfBriefcasesOpened = 3
 // Implement the primary logic of the problem here
 // Some output may be given here if you desire
 
+if countOfBriefcasesOpened == 0 {
+    print("Deal!")
+    exit(0)
+}
+
+if countOfBriefcasesOpened == 0 {
+    print("No deal.")
+    exit(0)
+}
+
+var moneyInBriefCases : [Int] = [0,100,500,1000,5000,10000,50000,100000,500000,1000000]
+var totalMoney = 0
+
 // Collect the list of what briefcases were opened here
+var totalInBriefCasesOpened = 0
 for counter in 1...countOfBriefcasesOpened {
-    
+    while 1  == 1{
     // Ask what briefcase was opened in this turn
     print("Briefcase opened, in turn \(counter), was:")
     
@@ -29,10 +62,34 @@ for counter in 1...countOfBriefcasesOpened {
         // If someone enters nil input, just skip to the next line
         continue
     }
+    guard let givenInteger = Int(givenInput) else{
+        continue
+    }
     
-    // What was provided?
-    print("The given input was: \(givenInput)")
+    if givenInteger < 0 || givenInteger > 10{
+        continue
+    }
     
-    // Implement the rest of your logic here...
+    // Implement the rest of your logic here...\
+    totalMoneyG += moneyInBriefCases][givenInteger]
+    }
+}
+
+while 1 == 1 {
+    print("What was the banker’s offer?")
+    
+    guard let givenInput = readLine() else{
+        //promt again if nil
+        continue
+    }
+    
+    guard let givenInteger = Int(givenInput) else{
+        continue
+    }
+    
+    if givenInteger < 10{
+        continue
+    }
+    break
 }
 
